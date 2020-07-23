b0efcbfd5b0fe7223656716e0187394d1256fa97
Viktorharmens <viktor@visualmasters.nl>
2020-07-23 10:04:10 +0200

    update composer and disabled error report

--- functions.php
+++ functions.php
@@ -15,8 +15,8 @@
 	// docker logs -f --since=$date $containerid >/dev/null
 
 	// Error reporting
-	error_reporting(E_ALL);
-	ini_set('display_errors', 'on');
+	// error_reporting(E_ALL);
+	// ini_set('display_errors', 'on');
 
 	// Initialize theme
 	include_once 'functions/init.php';
