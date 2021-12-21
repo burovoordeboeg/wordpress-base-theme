abdd93ddd1cd9a3ff16f756c3412d38c77bfd12b
bdfpplinsinga <daniel@burovoordeboeg.nl>
2021-12-21 11:50:23 +0100

    added swiper js

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -8,6 +8,10 @@ import './blocks';
 // usefull for simple state management
 import './alphine';
 
+// Import swiper js
+// Swiper.js is a Modern Mobile Touch Slider
+import './swiper';
+
 // Import styles
 import '../css/styles.css';
 import '../css/editor-styles.css';
