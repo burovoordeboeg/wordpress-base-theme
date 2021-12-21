9f6d848b772823c74fac7285c48dbc958a531beb
bdfpplinsinga <daniel@burovoordeboeg.nl>
2021-12-21 11:33:26 +0100

    Added alphine js

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -3,6 +3,11 @@ console.log('Hi')
 // Import blocks scripts
 import './blocks';
 
+// Import alphine js
+// Alphine js is new, lightweight, JavaScript framework thats really 
+// usefull for simple state management
+import './alphine';
+
 // Import styles
 import '../css/styles.css';
 import '../css/editor-styles.css';
