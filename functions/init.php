12657a43d32d660739c1889c0bfdbd2b74b27bdf
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-02-21 09:22:16 +0100

    Added comment for manifest

--- functions/init.php
+++ functions/init.php
@@ -83,6 +83,7 @@
 		
 		// Enqueue gutenberg assets
 		add_action('enqueue_block_editor_assets', 'enqueue_block_editor_assets');
+		
 		function enqueue_block_editor_assets() {
 			// wp_enqueue_style('fira-font', '//fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 			wp_enqueue_script('scripts-js', get_template_directory_uri() . '/dist/scripts.js');
@@ -113,7 +114,7 @@
 	// Removes core block support
 	remove_theme_support('core-block-patterns');
 
-	// Mix 
+	// Mix manifest function 
 	function mix($path, $manifestDirectory = '')
     {
         static $manifest;
