b7ce018eb992d63db63c2daa597a43856785559d
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-04-06 17:03:16 +0200

    Added a basic block structure

---
+++ blocks/wysiwyg/wysiwyg.php
@@ -0,0 +1,32 @@
+<?php
+
+	namespace BvdB\Blocks;
+
+	use BvdB\Gutenberg;
+	use BvdB\Gutenberg\Blocks as Blocks;
+
+	class Wysiwyg extends Blocks
+	{
+		/**
+		 * Register the block
+		 */
+		public function __construct()
+		{
+			PARENT::register_block( array(
+				'name' => 'wysiwyg',
+				'title' => 'Wysiwyg',
+				'description' => 'This is an wysiwyg block',
+				'category' => 'custom',
+				'icon' => 'admin-comments',
+				'keywords' => array( 'text' ),
+				'post_types' => array( 'post', 'page' ),
+				'mode' => 'preview',
+				'align' => array( 'wide', 'full',),
+				'multiple' => true,
+			));
+		}
+	}
+
+	// Call block-class
+	new Wysiwyg();
+?>
