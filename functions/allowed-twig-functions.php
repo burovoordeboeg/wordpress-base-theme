a9bff84d6f7655b47d34e5061e56d4f557814084
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-04-11 15:27:01 +0200

    added focal point in image text

--- functions/allowed-twig-functions.php
+++ functions/allowed-twig-functions.php
@@ -5,7 +5,7 @@
 
 	add_filter( 'bvdb_allowed_twig_functions', function ($functions) {
 		return array_merge( $functions, array(
-			'wp_get_attachment_image_src',
+			'wp_get_attachment_image_url',
 			'wp_get_attachment_image'
 		) );
 	} );
