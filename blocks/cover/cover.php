97dc328e53632ed4f32cf0de46c231b67c1a20f2
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-04-12 11:26:49 +0200

    Created cover block

---
+++ blocks/cover/cover.php
@@ -0,0 +1,33 @@
+<?php
+
+	namespace BvdB\Blocks;
+
+	use BvdB\Gutenberg;
+	use BvdB\Gutenberg\Blocks as Blocks;
+
+	class Cover extends Blocks
+	{
+		/**
+		 * Register the block
+		 */
+		public function __construct()
+		{
+			PARENT::register_block( array(
+				'name' => 'cover',
+				'title' => 'Cover',
+				'description' => 'This is an cover block',
+				'category' => 'custom',
+				'icon' => 'admin-comments',
+				'keywords' => array( 'image', 'text' ),
+				'post_types' => array( 'post', 'page' ),
+				'mode' => 'preview',
+				'align' => array('wide', 'full',),
+				'align_text' => false,
+				'multiple' => true,
+			));
+		}
+	}
+
+	// Call block-class
+	new Cover();
+?>
