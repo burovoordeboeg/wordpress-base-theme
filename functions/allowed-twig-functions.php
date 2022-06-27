60e1dd5c1331f1b0f17ca0b62764f4a0a597507a
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-06-27 10:16:14 +0200

    added bvdb_allowed_twig_functions

--- functions/allowed-twig-functions.php
+++ functions/allowed-twig-functions.php
@@ -1,10 +1,9 @@
 <?php
 
-    // Exit if accessed directly
-    if (!defined('ABSPATH')) exit;
+// Exit if accessed directly
+if (!defined('ABSPATH')) exit;
 
-	add_filter( 'bvdb_allowed_twig_functions', function ($functions) {
-		return array_merge( $functions, array(
-			'wp_get_attachment_image_url',
-		) );
-	} );
+add_filter('bvdb_allowed_twig_functions', function ($functions) {
+	$functions[] = 'wp_get_attachment_image_url';
+	return $functions;
+});
