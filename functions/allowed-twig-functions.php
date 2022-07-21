abc292812726a712902f7edf2ef3c7fb8559b8c9
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-07-21 15:12:29 +0200

    accepted incomming chnages

--- functions/allowed-twig-functions.php
+++ functions/allowed-twig-functions.php
@@ -10,11 +10,11 @@ add_filter('bvdb_allowed_twig_functions', function ($functions) {
 
 	$allowed_functions = array(
 		// Add array of allowed functions
-		wp_get_attachment_image_url,
-		wp_get_attachment_metadata,
-		wp_get_attachment_caption,
-		get_the_title,
-		get_post_meta
+		'wp_get_attachment_image_url',
+		'wp_get_attachment_metadata',
+		'wp_get_attachment_caption',
+		'get_the_title',
+		'get_post_meta'
 	);
 
 	// Return the array of current functions and add extra allowed functions
