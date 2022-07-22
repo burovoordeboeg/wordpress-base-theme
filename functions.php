7a5a9d249cb75d511252c95130a2d4b86013b178
jstreuper <justin@burovoordeboeg.nl>
2022-07-22 08:50:47 +0200

    Added innerblocks

--- functions.php
+++ functions.php
@@ -9,3 +9,4 @@ include_once 'functions/init.php';
 // Add theme specific methods
 include_once 'functions/allowed-twig-functions.php';
 include_once 'functions/context.php';
+include_once 'functions/functions.php';
