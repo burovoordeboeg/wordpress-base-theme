f91dfd61051fdfe93aa436a5a147579538ab2a7b
bdfpplinsinga <daniel@burovoordeboeg.nl>
2023-07-12 09:17:17 +0800

    Update block json files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.twig", "./blocks/**/*.twig", "./assets/**/*.js"],
-  theme: {
-    container: {
-      center: true,
-      padding: "1rem",
+    content: [
+        "./templates/**/*.twig",
+        "./blocks/**/*.twig",
+        "./assets/**/*.js",
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: "1rem",
+        },
+        extend: {
+            colors: {
+                primary: {
+                    100: "#fce6da",
+                    200: "#f9ccb5",
+                    300: "#f6b390",
+                    400: "#f3996b",
+                    500: "#f08046",
+                    600: "#c06638",
+                    700: "#904d2a",
+                    800: "#60331c",
+                    900: "#301a0e",
+                },
+            },
+        },
     },
-    extend: {},
-  },
-  corePlugins: {
-    aspectRatio: false,
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/container-queries"),
-  ],
+    corePlugins: {
+        aspectRatio: false,
+    },
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/container-queries"),
+    ],
 };
