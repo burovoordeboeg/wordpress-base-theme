c07862810ed97ca5a331942febbfc7d8da88a004
jstreuper <justin@burovoordeboeg.nl>
2025-03-28 15:27:57 +0100

    Added glob for images in images folder to add them to the compile on build.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,8 @@
+// Import the images to be used
+if (import.meta.env.PROD) {
+	import.meta.glob('@/images/**/*.{png,jpg,jpeg}', { eager: true });
+}
+
 // Import styles
 if (import.meta.env.DEV) {
 	// In development mode, import the CSS so that Vite can handle it (HMR, etc.)
